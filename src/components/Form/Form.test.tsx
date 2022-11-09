f079ed89079ab452082658392298a8f556ee14d8
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-11-09 09:51:09 +0100

    Created the Car interface, added a state in form and tested it

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Form from "./Form";
 
 describe("Given a Form component", () => {
@@ -21,6 +22,40 @@ describe("Given a Form component", () => {
       elementsOfForm.forEach((element) => {
         expect(element).toBeInTheDocument();
       });
+    });
+  });
+
+  describe("When the user type in the fields of the form", () => {
+    test("Then it should take the value of user types", async () => {
+      const usernameTyped = "Margarita";
+      const brandTyped = "Alfa Romeo";
+      const enrollmentDateTyped = "2020-03-02";
+      const fuelTyped = "G";
+
+      render(<Form />);
+
+      const placeholderInputUsername = "Enter your name :)";
+      const placeholderInputEnrollmentDate = "enrollmentDate";
+      const labelOfBrandCar = "Choose the car brand:";
+      const labelOfFuelType = "Fuel type:";
+
+      const inputOfUsername = screen.getByPlaceholderText(
+        placeholderInputUsername
+      ) as HTMLInputElement;
+      const inputOfEnrollmentDate = screen.getByPlaceholderText(
+        placeholderInputEnrollmentDate
+      ) as HTMLInputElement;
+      const selectOfBrandCar = screen.getByLabelText(labelOfBrandCar);
+      const selectOfFuel = screen.getByLabelText(labelOfFuelType);
+
+      await userEvent.type(inputOfUsername, usernameTyped);
+      await userEvent.type(inputOfEnrollmentDate, enrollmentDateTyped);
+      await userEvent.selectOptions(selectOfBrandCar, brandTyped);
+      await userEvent.selectOptions(selectOfFuel, fuelTyped);
+
+      expect(inputOfEnrollmentDate).toHaveValue(enrollmentDateTyped);
+      expect(selectOfBrandCar).toHaveValue(brandTyped);
+      expect(selectOfFuel).toHaveValue(fuelTyped);
     });
   });
 });

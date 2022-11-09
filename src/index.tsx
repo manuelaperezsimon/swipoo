1eeff9f0ee72e1c92c200c39af19924942d1b1c1
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-11-09 10:29:53 +0100

    Installed toastify, created a modals, instantiated ToastContainer

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ToastContainer />
     <BrowserRouter>
       <App />
     </BrowserRouter>

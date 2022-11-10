6f16ece26a489f7eb18068be285975bcfee98696
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-11-10 08:46:44 +0100

    Added a returns in getModelsCars and tested it

--- src/hooks/cars/useCars.test.ts
+++ src/hooks/cars/useCars.test.ts
@@ -3,6 +3,7 @@ import useCars from "./useCars";
 import axios from "axios";
 
 import { toast } from "react-toastify";
+import expectedListOfCars from "../../test-utils/cars";
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 jest.mock("axios");
@@ -29,6 +30,18 @@ describe("Given a useCars hook", () => {
           position: toast.POSITION.TOP_CENTER,
         }
       );
+    });
+  });
+  describe("When the getModelsCars it's invoked and the get was ok", () => {
+    test("Then it should return a list of cars", async () => {
+      mockedAxios.get.mockResolvedValue({ data: expectedListOfCars });
+      let result;
+
+      await act(async () => {
+        result = await getModelsCars("Alfa Romeo", "2015/05/06", "G");
+      });
+
+      expect(result).toStrictEqual(expectedListOfCars.cars);
     });
   });
 });

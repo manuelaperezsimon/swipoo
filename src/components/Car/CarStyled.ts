23d56a83da14e8a7fbca8d9835b51f4b678f9d71
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-11-10 11:09:29 +0100

    Created a basic style of car component

---
+++ src/components/Car/CarStyled.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const CarStyled = styled.div`
+  .table__container {
+    width: 100%;
+    text-align: left;
+  }
+
+  table {
+    width: 100%;
+  }
+
+  th,
+  td {
+    width: 25%;
+  }
+`;
+
+export default CarStyled;

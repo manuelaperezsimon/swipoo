7f98109864de62f1662fa9d45bca869b058a9c66
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-11-13 19:46:37 +0100

    Added a key in CarsList

--- src/components/CarsList/CarsList.tsx
+++ src/components/CarsList/CarsList.tsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import { CompleteCarInfo } from "../../interfaces/interfaceCar";
 import CarCard from "../Car/Car";
 import CarsListStyled from "./CarsListStyled";
+import { v4 as uuidv4 } from "uuid";
 
 const CarsList = (): JSX.Element => {
   const initialCarsListState: CompleteCarInfo[] = [];
   const [carsList, setCarsList] = useState(initialCarsListState);
+  const key = uuidv4();
 
   useEffect(() => {
     const cars = localStorage.getItem("carsCreated");
@@ -21,7 +23,7 @@ const CarsList = (): JSX.Element => {
         {carsList.length > 0 ? (
           <ul className="cars__list">
             {carsList.map((car) => (
-              <li key={car.user + car.brand + car.model} className="cars__car">
+              <li key={key} className="cars__car">
                 <CarCard car={car} />
               </li>
             ))}
